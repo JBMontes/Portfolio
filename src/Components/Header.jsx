789eef1f2c650d020f49ad367a41e4f5f164d3a4
Jeremy Montes <jeremymontes@pursuit.org>
2024-06-29 18:36:56 -0400

    first commit

---
+++ src/Components/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header>
+            <button> Project</button>
+
+             <img src="/Peru.jpeg" height="300px"></img>
+
+             <button>Links</button>
+        </header>
+    );
+};
+
+export default Header;
---
+++ src/Pages/Landing.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import "../Styles/LandingPage.css"
+const Landing = () => {
+    return (
+        <div className="home">
+      
+        </div>
+    );
+};
+
+export default Landing;
