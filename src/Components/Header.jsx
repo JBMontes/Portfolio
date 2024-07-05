7a41c99a6e3af6b642f1c31b84476d0972290042
Jeremy Montes <jeremymontes@pursuit.org>
2024-07-05 14:49:31 -0400

    made css edits to pages, changed button format in header

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,22 +1,39 @@
-import React from 'react';
+import { useState } from "react";
 import { useNavigate } from 'react-router-dom';
 import useScrollToTop from './useScrollToTop';
 import "../Styles/Header.css"
 
 const Header = () => {
+    const [pageKey, setPageKey] = useState("/");
     let nav = useNavigate()
     useScrollToTop();
+
+    const handlePageChange = (e) => {
+        const selectedPage = e.target.value;
+        setPageKey(selectedPage);
+        nav(selectedPage); 
+    }
+
+    const handleLogoChange = (e) => {
+        const selectedPage = "/";
+        setPageKey(selectedPage);
+        nav(selectedPage); 
+    }
 
     return (
         <header className="header">
             <div>
-                <h3 className="logo" onClick={() => nav("/")}>JBM</h3>
+                <h3 className="logo" onClick={handleLogoChange}>JBM</h3>
             </div>
 
-            <div className="button">
-            <button onClick={() => nav('/home')}> Home</button>
-                <button onClick={() => nav('/projects')}> Projects</button>
-                <button onClick={() => nav('/resume')}>Resume</button>
+            <div className="headerButton">
+
+<select value={pageKey} onChange={handlePageChange}>
+          <option value="/">Welcome</option>
+          <option value="/home">Home</option>
+          <option value="/projects">Projects</option>
+          <option value="/resume">Resume</option>
+        </select>
             </div>
         </header>
     );
--- src/Pages/Landing.jsx
+++ src/Pages/Landing.jsx
@@ -5,8 +5,10 @@ import '../Styles/Landing.css'
 const Landing = () => {
     return (
         <div className="landingBody">
+            <div className="logoAndButton">
             <img src={"/jmLogo.png"} alt="Jeremy Montes Logo" />
             <Link to="/home"><button>Enter</button></Link>
+            </div>
         </div>
     );
 };
